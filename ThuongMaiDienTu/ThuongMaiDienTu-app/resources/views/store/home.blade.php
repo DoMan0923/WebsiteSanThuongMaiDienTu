a7d646fd5136fcdc895ddb5896123092d1c3eb7c
dohueman.github.io <dohueman.ltp20@gmail.com>
2024-03-01 18:12:37 +0700

    San thuong mai dien tu

---
+++ ThuongMaiDienTu/ThuongMaiDienTu-app/resources/views/store/home.blade.php
@@ -0,0 +1,48 @@
+{{-- @extends('layouts.app')
+@section('main')
+<link rel="stylesheet" href="{{ asset('owlcarousel/assets/owl.carousel.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('owlcarousel/assets/owl.theme.default.min.css')}}" type="text/css">
+<style>
+</style>
+<div class="owl-carousel owl-theme">
+    <div class="item"><h4>143434343</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+    <div class="item"><h4>5</h4></div>
+    <div class="item"><h4>6</h4></div>
+    <div class="item"><h4>7</h4></div>
+    <div class="item"><h4>8</h4></div>
+    <div class="item"><h4>9</h4></div>
+    <div class="item"><h4>10</h4></div>
+    <div class="item"><h4>11</h4></div>
+    <div class="item"><h4>12</h4></div>
+</div>
+
+<script src="{{ asset('owlcarousel/owl.carousel.min.js')}}"></script>
+<script>
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            navText: [`<img  src="${URL_HOST}FE/img/core-img/chevron-left.svg" class="chevron-left" alt="">`, `<img class="chevron-right" src="${URL_HOST}FE/img/core-img/chevron-right.svg" alt="">`],
+            dots:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
+</script>
+@endsection --}}
+
+@extends('layouts.layout_store')
+@section('content')
+fdsfd
+@endsection
